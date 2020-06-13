ae73fb79c47d9770b0979376be9621523d7e3e4f
adityainamdar100 <40563668+adityainamdar100@users.noreply.github.com>
2020-06-13 20:31:37 +0530

    LeetCode June Challenge - Day 13 using JS

---
+++ Day13.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var largestDivisibleSubset = function(nums) {
+     if ( ! nums.length ) return []
+    nums.sort((a,b)=>a-b)
+    var dp = Array.from({length:nums.length},(_,i)=>[nums[i]])
+
+    for(let i=nums.length-1;i>=0;i--){
+        for ( let j = i + 1; j < nums.length; j++ ){
+            if ( dp[i].length <= dp[j].length && dp[j][0] % nums[i] === 0 ) {
+                dp[i] = [ nums[i], ...dp[j] ]
+            }
+        }
+    }
+
+    return dp.reduce( (a,x) => a.length > x.length ? a : x )
+};
